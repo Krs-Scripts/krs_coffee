58a44bbd9f7c93364db56cbe4dae0b710b122575
Dev-Shaz <63104021+Dev-Shaz@users.noreply.github.com>
2023-08-17 11:52:54 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,13 +1,25 @@
 ESX = exports.es_extended:getSharedObject()
 
+function LegitItemPrice(item, price)
+    for k,v in pairs(Krs.Items) do 
+        if v.nomeItems == item then 
+            if price == v.price then 
+                return true 
+            end 
+        end
+    end
+    return false 
+end
 
 RegisterNetEvent("krs-shop:addItem", function(item)
     local source = source
     local ox_inventory = exports.ox_inventory
 
     if item.nomeItems and item.price then
-        print(item.nomeItems)
-        print(item.price)
+        if not LegitItemPrice(item.nomeItems, item.price) then 
+            -- PUT AC BAN TRIGGER HERE
+            return print("[KRS_COFFEE] " .. GetPlayerName(source) .. " is exploiting." .. " | Item: " .. item.nomeItems .. " | price: " .. item.price) 
+        end
         ox_inventory:AddItem(source, item.nomeItems, 1)
 
         ox_inventory:RemoveItem(source, "money", item.price)
